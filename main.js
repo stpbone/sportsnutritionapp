6bbc4225ee5ca998bb7aa555395248e722317cdc
Simon Bone <32798419+stpbone@users.noreply.github.com>
2018-11-28 20:29:29 +0000

    Updated functionality

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
+// food calorie values
+const boiledEggs = 156
+const toastTwoSlicesWhiteJam = 171
+const porridgeWithHoneyBanana = 250
+const whiteCoffee = 79
+const avocadoSalad = 312
+const miniSoreen = 92
+const chickenStirFry = 200
 
-function calorieIntake(burned, consumed) {
-    return 2500 - consumed + burned;
+// Enter what you had for breakfast
+var breakfast = boiledEggs + toastTwoSlicesWhiteJam + porridgeWithHoneyBanana
+
+// Enter what you had for lunch 
+var lunch = avocadoSalad
+
+// Enter what you had for dinner
+var dinner = chickenStirFry
+
+// Calculates snacks consumed 
+var snacks = (2*miniSoreen) + (4*whiteCoffee)
+
+// Calculates total calorie consumption for the day
+var totalCals = lunch + breakfast + dinner + snacks
+
+// Calories burned accoring to Strava/TrainerRoad/Garmin data
+var calsBurned = 100
+
+// Log the outputs to the terminal
+console.log('Today your breakfast was ' + breakfast + ' calories')
+console.log('Today your lunch was ' + lunch + ' calories')
+console.log('Today your dinner was ' + dinner + ' calories')
+console.log('Today your snack consumption was ' + snacks + ' calories')
+console.log('Today your total calorie intake was: ' + totalCals)
+
+// Function which calculates calculates the number of calories burned and consumed in the day, and substracts from the daily allowance
+function calorieIntake() {
+    return 1800 - totalCals + calsBurned;
 }
 
-const calorieBalanceToday = calorieIntake(800, 4000)
+const calorieBalanceToday = calorieIntake()
 
 if (calorieBalanceToday < 0) {
     console.log('Your calorie balance is ' + calorieBalanceToday + ' ...you have had too many calories today fatboy!')
